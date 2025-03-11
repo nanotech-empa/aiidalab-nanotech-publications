723e7fa04efd582704281f57074d9717f0b2c73c
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2025-03-11 09:51:26 +0000

    draft

--- start.py
+++ start.py
@@ -5,8 +5,9 @@ def get_start_widget(appbase, jupbase, notebase):
     return ipw.HTML(
         f"""
     <div align="center">
-        <a href="{appbase}/coffee.ipynb" target="_blank">
-            <img src="https://github.com/nanotech-empa/aiidalab-nanotech-coffee/blob/main/logo.png?raw=true" height="120px" width=243px">
+        <a href="{appbase}/publications.ipynb" target="_blank">
+            <img src="https://github.com/nanotech-empa/aiidalab-nanotech-publications/blob/main/logo.png?raw=true" height="120px" width=243px">
         </a>
     </div>"""
     )
+    
